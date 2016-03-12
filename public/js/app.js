e11a8251fd48c30d4a089bfa09a3fbdad2fe2ef1
93lucasp <spezzanoluca@gmail.com>
2016-03-12 15:15:33 -0800

    modal sign up created

---
+++ public/js/app.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+
+
+
+  //CREATE SONGS
+$('#modalButton').on('click', function() {
+
+    //USES BOOTSTRAP/jQUERY TO OPEN THE MODAL
+    $('#triggerModal').modal();
+   
+
+
+});
+});
