3169bd2c835110c6b0cbf8995ec640aec7af6fb9
93lucasp <spezzanoluca@gmail.com>
2016-03-13 12:40:59 -0700

    facebook login done

--- config/routes.js
+++ config/routes.js
@@ -1,10 +1,14 @@
-var express = require('express'),
-	app = express(),
-	mongoose = require('mongoose'),
-	bodyParser = require('body-parser'),
-	methodOverride = require('method-override'),
-	path = require('path'),
-	logger = require('morgan');
+var express 		= require('express'),
+	app 			= express(),
+	mongoose 		= require('mongoose'),
+	bodyParser 		= require('body-parser'),
+	methodOverride 	= require('method-override'),
+	path 			= require('path'),
+	logger 			= require('morgan');
+	expressSession 	= require('express-session');
+	cookieParser   	= require("cookie-parser");
+	passport       	= require('passport');
+	// usersController = require('../controllers/users');
 
 var	router = express.Router();
 
@@ -14,8 +18,6 @@ mongoose.connect('mongodb://localhost/friendme');
 router.get('/', function(req, res) {
 	res.render("./partials/welcome");
 });
-
-
 
 
 
