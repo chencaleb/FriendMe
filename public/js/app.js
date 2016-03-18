fa22b6c0dfe3db0bd7ba852821958ad97a87fafc
93lucasp <spezzanoluca@gmail.com>
2016-03-17 20:03:39 -0700

    changes in welcome page

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,15 @@
 $(document).ready(function() {
+
+  var jumboHeight = $('.jumbotron').outerHeight();
+function parallax(){
+    var scrolled = $(window).scrollTop();
+    $('.bg').css('height', (jumboHeight-scrolled) + 'px');
+}
+
+$(window).scroll(function(e){
+    parallax();
+});
+  
   $("#toggleEditBtn").click(function() {
     $("#editForm").toggle();
   });
